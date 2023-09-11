6d38b1f2a63b26d90c1534794fd369478850d237
shrey_akan <shereyansjain0904@gmail.com>
2023-09-11 01:13:00 -0700

    backend1

---
+++ src/main/java/com/youtube/jwt/entity/User.java
@@ -0,0 +1,93 @@
+package com.youtube.jwt.entity;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class User {
+
+    @Id
+    private String userName;
+    private String userFirstName;
+    private String userLastName;
+    private String userPassword;
+    private String companyuser;
+    private String websiteuser;
+    private String userphone;
+    private String usercountry;
+    private String userstate;
+    private String usercity;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(name = "USER_ROLE",
+            joinColumns = {
+                    @JoinColumn(name = "USER_ID")
+            },
+            inverseJoinColumns = {
+                    @JoinColumn(name = "ROLE_ID")
+            }
+    )
+    private Set<Role> role;
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserFirstName() {
+		return userFirstName;
+	}
+	public void setUserFirstName(String userFirstName) {
+		this.userFirstName = userFirstName;
+	}
+	public String getUserLastName() {
+		return userLastName;
+	}
+	public void setUserLastName(String userLastName) {
+		this.userLastName = userLastName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public String getCompanyuser() {
+		return companyuser;
+	}
+	public void setCompanyuser(String companyuser) {
+		this.companyuser = companyuser;
+	}
+	public String getWebsiteuser() {
+		return websiteuser;
+	}
+	public void setWebsiteuser(String websiteuser) {
+		this.websiteuser = websiteuser;
+	}
+	public String getUserphone() {
+		return userphone;
+	}
+	public void setUserphone(String userphone) {
+		this.userphone = userphone;
+	}
+	public String getUsercountry() {
+		return usercountry;
+	}
+	public void setUsercountry(String usercountry) {
+		this.usercountry = usercountry;
+	}
+	public String getUserstate() {
+		return userstate;
+	}
+	public void setUserstate(String userstate) {
+		this.userstate = userstate;
+	}
+	public Set<Role> getRole() {
+		return role;
+	}
+	public void setRole(Set<Role> role) {
+		this.role = role;
+	}
+	
+
+   
+}
